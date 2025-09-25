7503577233a671beb46a7af9abc45f3530821e78
coder-noman <abdhullaalnoman25@gmail.com>
2025-09-25 11:20:31 +0600

    first

--- app/script.js
+++ app/script.js
@@ -23,23 +23,28 @@ if (logoutBtn) {
 //Handle Logout Button End
 
 //Declare psu array and variable
-let fan_arr = [0, 0, 0, 0, 0, 0, 0, 0];
+// let fan_arr = [10, 20, 30, 40, 20];
+// let fan_arr = [0, 0, 0, 0, 0];
+let fan_arr = [0, 10, 0, 20, 0];
 
 // Chart array and variable Declare
-let temp = [0, 0, 0, 0, 0, 0, 0, 0];
-let hum = [0, 0, 0, 0, 0, 0, 0, 0];
+let ammonia = [10, 7, 30];
+let ph = [10, 20, 5];
 
-let barChart;
-let lineChart;
+let barChart1;
+let barChart2;
 
 // Default Data Show Start
-updateAllData(0, 0, 0, 0, 0, 0);
-updateBarChart();
+updateAllData(10, 20, 30, 40, 50, 60);
+updateBarChart1();
+updateBarChart2() 
 fanDataShow();
 // Default Data Show end
+// Hams_HO::::GD,235,235,0,0.0,0.0,0,1,1,1,1,1,192.168.0.87,Gra,45,4.2,1,1,0
 
 //.........websocket_client code Start..............
-var socket = new WebSocket("ws://27.147.170.162:81");
+// var socket = new WebSocket("ws://27.147.170.162:81");
+var socket = new WebSocket("ws://27.147.170.162:8");
 socket.onmessage = function (event) {
   const data = event.data.split(":");
   const data_catagory = data[0] || "";
@@ -71,9 +76,6 @@ socket.onmessage = function (event) {
     splited_data[6]
   );
 
-  // Line chart Data
-  updateLineChart(splited_data[5], splited_data[6]);
-
   // Device Inforfation
   deviceInformation(
     splited_data[12],
@@ -103,29 +105,32 @@ function psuDataInsert(x, y, z) {
 }
 function fanDataShow() {
   const fanId = ["fan1", "fan2", "fan3", "fan4", "fan5"];
+  const fanIcon = ["fan1-icon", "fan2-icon", "fan3-icon", "fan4-icon", "fan5-icon"];
   const fanDisplayId = ["fan1-d", "fan2-d", "fan3-d", "fan4-d", "fan5-d"];
   const fanCardData = ["Fan1", "Fan2", "Fan3", "Fan4", "Fan5"];
   // ipdu 1 Data show
-  for (i = 0, j = 0; i <= 7; i++, j++) {
+  for (i = 0; i <= 4; i++) {
     if (fan_arr[i] >= 1) {
-      fanOnShowData(fanId[j], fanDisplayId[j], fan_arr[i]);
+      fanOnShowData(fanId[i], fanDisplayId[i], fan_arr[i],fanIcon[i]);
     } else {
-      fanOffShowData(fanId[j], fanCardData[j]);
+      fanOffShowData(fanId[i], fanCardData[i],fanIcon[i]);
     }
   }
 }
 
 //Psu On Show Data Funtion
-function fanOnShowData(fan_Id, fan_d_id, fan_value) {
+function fanOnShowData(fan_Id, fan_d_id, fan_value,fan_icon) {
   document.getElementById(fan_Id).innerText = "ON";
   document.getElementById(fan_Id).classList.add("on-btn");
+  document.getElementById(fan_icon).classList.add("active-fan");
   document.getElementById(fan_d_id).innerText = `${fan_value} VA`;
   document.getElementById(fan_d_id).classList.add("show-btn");
 }
 //Psu Off Show Data Funtion
-function fanOffShowData(fan_Id, fanCardData) {
+function fanOffShowData(fan_Id, fanCardData,fan_icon) {
   document.getElementById(fan_Id).innerText = "OFF";
   document.getElementById(fan_Id).classList.add("off-btn");
+  document.getElementById(fan_icon).classList.remove("active-fan");
   let ul = document.getElementById("alert-list");
   let li = document.createElement("li");
   li.classList.add("alert-list-card");
@@ -287,100 +292,100 @@ function updateGauge(elementId, value, ranges) {
 
 //update all gauge data
 function updateAllData(a, b, c, d, e, f) {
+  console.log(a,b,c,d,e,f)
   // Input Voltage (0-300V)
-  const inputVoltage = a;
-  updateGauge("input-voltage", inputVoltage, {
-    green: [191, 245],
-    orange: [0, 190],
-    red: [246, 300],
-    max: 300,
-  });
-
-  // Alert for inputVoltage
-  if (inputVoltage >= 0 && inputVoltage <= 190) {
-    gaugeAlert("Input Voltage", "low");
-  } else if (inputVoltage >= 246 && inputVoltage <= 300) {
-    gaugeAlert("Input Voltage", "high");
-  }
-
-  // UPS1 Output Voltage (0-300V)
-  const ups1Voltage = b;
-  updateGauge("ups1-voltage", ups1Voltage, {
-    green: [211, 230],
-    orange: [0, 210],
-    red: [231, 300],
-    max: 300,
-  });
-
-  // Alert for Ups1
-  if (ups1Voltage >= 0 && ups1Voltage <= 210) {
-    gaugeAlert("UPS1 Voltage", "low");
-  } else if (ups1Voltage >= 231 && ups1Voltage <= 300) {
-    gaugeAlert("UPS1 Voltage", "high");
-  }
-
-  // UPS2 Output Voltage (0-300V)
-  const ups2Voltage = c;
-  updateGauge("ups2-voltage", ups2Voltage, {
-    green: [211, 230],
-    orange: [0, 210],
-    red: [231, 300],
-    max: 300,
-  });
-
-  // Alert for Ups2
-  if (ups2Voltage >= 0 && ups2Voltage <= 210) {
-    gaugeAlert("UPS2 Voltage", "low");
-  } else if (ups2Voltage >= 231 && ups2Voltage <= 300) {
-    gaugeAlert("UPS2 Voltage", "high");
-  }
-
-  // Battery Voltage (0-60V)
-  const batteryVoltage = d;
-  updateGauge("battery-voltage", batteryVoltage, {
-    green: [241, 280],
-    orange: [221, 240],
-    red: [0, 220],
-    max: 280,
-  });
-
-  // Alert for Battery Voltage
-  if (batteryVoltage >= 221 && batteryVoltage <= 240) {
-    gaugeAlert("Battery Voltage", "low");
-  } else if (batteryVoltage >= 0 && batteryVoltage <= 220) {
-    gaugeAlert("Battery Voltage", "very Low");
-  }
-
-  // Temperature (0-55°C)
-  const temperature = e;
-  updateGauge("temperature", temperature, {
+  const temperature1 = a;
+  updateGauge("temperature1", temperature1, {
     green: [0, 25],
     orange: [26, 31],
     red: [32, 55],
     max: 55,
   });
 
-  // Alert for Temperature
-  if (temperature >= 26 && temperature <= 31) {
-    gaugeAlert("Temperature", "high");
-  } else if (temperature >= 32 && temperature <= 55) {
-    gaugeAlert("Temperature", "very high");
+ if (temperature1 >= 26 && temperature1 <= 31) {
+    gaugeAlert("Temperature-1", "high");
+  } else if (temperature1 >= 32 && temperature1 <= 55) {
+    gaugeAlert("Temperature-1", "very high");
   }
 
-  // Humidity (0-100%)
-  const humidity = f;
-  updateGauge("humidity", humidity, {
+  // UPS1 Output Voltage (0-300V)
+  const temperature2 = b;
+  updateGauge("temperature2", temperature2, {
+    green: [0, 25],
+    orange: [26, 31],
+    red: [32, 55],
+    max: 55,
+  });
+
+  // Alert for Ups1
+ if (temperature2 >= 26 && temperature2 <= 31) {
+    gaugeAlert("Temperature-2", "high");
+  } else if (temperature2 >= 32 && temperature2 <= 55) {
+    gaugeAlert("Temperature-2", "very high");
+  }
+
+  // UPS2 Output Voltage (0-300V)
+  const temperature3 = c;
+  updateGauge("temperature3", temperature3, {
+    green: [0, 25],
+    orange: [26, 31],
+    red: [32, 55],
+    max: 55,
+  });
+
+  // Alert for Ups2
+ if (temperature3 >= 26 && temperature3 <= 31) {
+    gaugeAlert("Temperature-3", "high");
+  } else if (temperature3 >= 32 && temperature3 <= 55) {
+    gaugeAlert("Temperature-3", "very high");
+  }
+
+  // Battery Voltage (0-60V)
+  const temperature4 = d;
+  updateGauge("temperature4", temperature4, {
+    green: [0, 25],
+    orange: [26, 31],
+    red: [32, 55],
+    max: 55,
+  });
+
+  // Alert for Battery Voltage
+ if (temperature4 >= 26 && temperature4 <= 31) {
+    gaugeAlert("Temperature-4", "high");
+  } else if (temperature4 >= 32 && temperature4 <= 55) {
+    gaugeAlert("Temperature-4", "very high");
+  }
+
+  // Temperature (0-55°C)
+  const humidity1 = e;
+  updateGauge("humidity1", humidity1, {
     green: [41, 80],
     orange: [81, 100],
     red: [0, 40],
     max: 100,
   });
 
-  // Alert for Humidity
-  if (humidity >= 0 && humidity <= 40) {
-    gaugeAlert("Humidity", "low");
-  } else if (humidity >= 81 && humidity <= 100) {
-    gaugeAlert("Humidity", "high");
+  // Alert for humidity1
+  if (humidity1 >= 0 && humidity1 <= 40) {
+    gaugeAlert("humidity 1", "low");
+  } else if (humidity1 >= 81 && humidity1 <= 100) {
+    gaugeAlert("humidity 1", "high");
+  }
+
+  // humidity1 (0-100%)
+  const humidity2 = f;
+  updateGauge("humidity2", humidity2, {
+    green: [41, 80],
+    orange: [81, 100],
+    red: [0, 40],
+    max: 100,
+  });
+
+  // Alert for humidity1
+  if (humidity2 >= 0 && humidity2 <= 40) {
+    gaugeAlert("humidity 2", "low");
+  } else if (humidity2 >= 81 && humidity2 <= 100) {
+    gaugeAlert("humidity 2", "high");
   }
 }
 // gauge data end
@@ -390,16 +395,70 @@ let color = "white";
 
 // Initialize charts on page load
 function initializeCharts() {
-  // Voltage Chart (Bar Chart)
-  const voltageCtx = document.getElementById("voltage-chart").getContext("2d");
-  barChart = new Chart(voltageCtx, {
+  // Amonia Chart (Bar Chart)
+  const amoniaChart = document.getElementById("amonia-chart").getContext("2d");
+  barChart1 = new Chart(amoniaChart, {
     type: "bar",
     data: {
-      labels: ["Ipdu1", "Ipdu2", "Ipdu3"],
+      labels: ["A1", "A2", "A3"],
       datasets: [
         {
           label: "Load (VA)",
-          data: ipduSum_arr,
+          data: ammonia,
+          backgroundColor: [
+            "rgba(78, 205, 196, 0.7)",
+            "#fc5c6491",
+            "#3a67d1af",
+          ],
+          borderRadius: 10,
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      plugins: {
+        legend: {
+          display: false,
+        },
+      },
+      scales: {
+        y: {
+          beginAtZero: true,
+          grid: {
+            color: "rgba(160, 174, 192, 0.1)",
+          },
+          ticks: {
+            color: `${color}`,
+            font: {
+              size: 12,
+            },
+          },
+        },
+        x: {
+          grid: {
+            display: false,
+          },
+          ticks: {
+            color: `${color}`,
+            font: {
+              size: 12,
+            },
+          },
+        },
+      },
+    },
+  });
+  // PH Chart (Bar Chart)
+  const phChart = document.getElementById("ph-chart").getContext("2d");
+  barChart2 = new Chart(phChart, {
+    type: "bar",
+    data: {
+      labels: ["Ph1", "Ph2", "Ph3"],
+      datasets: [
+        {
+          label: "Load (VA)",
+          data: ph,
           backgroundColor: [
             "rgba(78, 205, 196, 0.7)",
             "#fc5c6491",
@@ -449,13 +508,20 @@ function initializeCharts() {
 // Initialize charts when the page loads
 window.addEventListener("load", initializeCharts);
 
-// update Bar chart
-function updateBarChart() {
-  console.log("ipduSum_arr update= ", ipduSum_arr);
+// update Bar chart 1
+function updateBarChart1() {
   // Update chart
-  if (barChart) {
-    barChart.data.datasets[0].data = ipduSum_arr;
-    barChart.update("none");
+  if (barChart1) {
+    barChart1.data.datasets[0].data = ammonia;
+    barChart1.update("none");
+  }
+}
+// update Bar chart 2
+function updateBarChart2() {
+  // Update chart
+  if (barChart2) {
+    barChart2.data.datasets[0].data = ph;
+    barChart2.update("none");
   }
 }
 // Chart data end
